a7092163143fc7a1005c9e0c7c11f24b506245cf
Andreas Ek <andreas@elseif.se>
2018-03-25 17:41:21 +0200

    Adding a basic test

---
+++ tests/Template/BasicTemplateTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Test\Newsflow\WP\Template;
+
+use Newsflow\WP\Helper\Template;
+
+class BasicTemplateTest extends \WP_UnitTestCase
+{
+
+    public function testAdminPageRender()
+    {
+        $output = Template::render('admin/settings/page', [
+            'headline' => 'This is the Headline',
+            'body' => 'This is the body',
+        ]);
+
+        $valid = strpos($output, 'This is the Headline');
+        $this->assertTrue($valid > 0);
+
+        $valid = strpos($output, 'This is the body');
+        $this->assertTrue($valid > 0);
+    }
+
+}
